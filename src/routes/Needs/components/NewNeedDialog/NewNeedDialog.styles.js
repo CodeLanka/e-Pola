43d14833e71b785065d26177468dc144b57711ed
agentmilindu <agentmilindu@gmail.com>
2020-04-04 08:12:09 +0530

    Fix styles issue

--- src/routes/Needs/components/NewNeedDialog/NewNeedDialog.styles.js
+++ src/routes/Needs/components/NewNeedDialog/NewNeedDialog.styles.js
@@ -1,16 +1,14 @@
 export default (theme) => ({
   root: {
     padding: theme.spacing(2),
-    width: '500px'
+    width: '500px',
+    maxWidth: '100%'
   },
   inputs: {
     ...theme.flexColumnCenter
   },
   selection: {
-    width: '50%',
-    [theme.breakpoints.up(600)]: {
-      width: '100%'
-    }
+    width: '100%'
   },
   buttons: {
     ...theme.flexColumnCenter
