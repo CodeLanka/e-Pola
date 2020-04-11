a88b58b326f14a2e26053357629d67247ed03329
agentmilindu <agentmilindu@gmail.com>
2020-04-11 13:36:29 +0530

    Add React i18n improvements

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,14 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import translations from 'lang/translations'
+import { env as environment } from 'config'
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: translations,
     lng: 'si',
-    debug: true,
+    debug: environment === 'dev',
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
